4dbf2d5496655298f368dd8d1ef20a8f707e9c20
Dörte de Kok <me@doerte.eu>
2022-05-06 17:12:52 +0200

    implemented perfect split and renamed "ignore" to "absolute", fixes #2

--- model.py
+++ model.py
@@ -43,17 +43,18 @@ except:
 
 categorical_features = ["wordclass"]
 continuous_features = ["freq", "image"]
-ignore_features = ["correct"]
+absolute_features = ["correct"]
 
 
-def run_all(data, n_sets, ignore, categorical, continuous, i):
+def run_all(data, n_sets, absolute, categorical, continuous, i):
     sign = False
 
     #get data from file
-    dat = prepare_data(data, ignore, continuous, categorical)
+    dat = prepare_data(data, absolute, continuous, categorical)
 
     #form clusters
     clusters = []
+   
     for df in dat:
         clusters_df = clustering(df)
         clusters.append(clusters_df)
@@ -71,7 +72,7 @@ def run_all(data, n_sets, ignore, categorical, continuous, i):
     #run again if sets are not different enough (max. 20 times)
     if sign and i < 20:
         ind = i + 1
-        run_all(data, n_sets, ignore, categorical, continuous, ind)
+        run_all(data, n_sets, absolute, categorical, continuous, ind)
     
     #give up trying after 20 runs
     elif sign:
@@ -110,39 +111,37 @@ def run_all(data, n_sets, ignore, categorical, continuous, i):
             f.write(stat_string)
             f.close()
 
-        #BUG: seems like items area in more than 1 set... first 15 double than 4 single, 20-35 missing when grouping by "ignore" -revisit grouping
-
-
-def prepare_data(data, ignore, continuous, categorical):
+def prepare_data(data, absolute, continuous, categorical):
     #remove first column (item name)
     data = data.drop(data.columns[[0]], axis=1)
-    data = data.drop(columns=ignore) #only nec if not grouped first
+    #data = data.drop(columns=absolute) #only nec if not grouped first
    
-    # split by "ignore" feature and remove ignored features from clustering
-    #grouped = data.groupby('correct')
+    #split by "absolute" feature and remove absolute features from clustering
+    grouped = data.groupby(absolute)
+
     data_transformed = []
 
-    #for name, group in grouped:
-        # drop ignored columns from further analysis
-    #    data_x = group.drop(columns=ignore)
+    for name, group in grouped:
+        # drop absolute columns from further analysis
+        data_x = group.drop(columns=absolute)
         # dummy-code categorical
 
         # transform data for fair comp continuous/categorical
-    #    mms = MinMaxScaler()
-    #    data_x[continuous] = mms.fit_transform(data_x[continuous])
-    #   data_transformed.append(data_x)
+        mms = MinMaxScaler()
+        data_x[continuous] = mms.fit_transform(data_x[continuous])
 
-    #return data_transformed
+        data_transformed.append(data_x)
 
-    #only necessary if not grouped by ignore feature
-    mms = MinMaxScaler()
-    data[continuous] = mms.fit_transform(data[continuous])
-    data_transformed.append(data)
+    ##only necessary if not grouped by absolute feature
+    #mms = MinMaxScaler()
+    #data[continuous] = mms.fit_transform(data[continuous])
+    #data_transformed.append(data)
 
     return data_transformed
 
 def clustering(transformed_data):
     cl_range = range(2, len(transformed_data))
+    
     # k means determine k
     largest_sil = (0, 0)
     clusters = []
@@ -154,6 +153,7 @@ def clustering(transformed_data):
         if sil > largest_sil[1]:
             largest_sil = (k, sil)
     km_final = KMeans(n_clusters=largest_sil[0], init='k-means++', n_init=1)
+
     pred_cluster = km_final.fit_predict(transformed_data)
 
     for k in range(0, largest_sil[0]):
@@ -162,8 +162,15 @@ def clustering(transformed_data):
     for item in range(0, len(pred_cluster)):
         clusters[pred_cluster[item]].append(item)
 
-    #print(clusters)
-    return clusters
+    final_clusters = []
+
+    for cluster in clusters:
+        cluster_new = []
+        for item in cluster:
+            cluster_new.append(transformed_data.iloc[item].name)
+        final_clusters.append(cluster_new)
+
+    return final_clusters
 
 
 def divide_in_sets(clusters, n_sets):
@@ -179,6 +186,7 @@ def divide_in_sets(clusters, n_sets):
 
 
 def statistics(data, sets, features):
+    #statistics are still carried out over whole set, not over subparts accorind to absolute criterion
     stats = []
 
     for feat in features:
@@ -195,6 +203,6 @@ def statistics(data, sets, features):
     return stats
 
 if no_sets > 1:
-    run_all(inputD, no_sets, ignore_features, categorical_features, continuous_features, 0)
+    run_all(inputD, no_sets, absolute_features, categorical_features, continuous_features, 0)
 else:
     print("Please use more than 1 set for this tool to be meaningful!")
